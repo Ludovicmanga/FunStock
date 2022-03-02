e1eb5e1dde6f0fe683373b7f663073b5f58f0cf2
Ludovic <ludovic.mangaj@gmail.com>
2022-03-02 11:46:10 +0100

    we protected the home route in security yaml

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -40,7 +40,7 @@ class MainController extends AbstractController
         $this->userService = $userService;
     }
 
-    #[Route('/', name: 'home')]
+    #[Route('/home', name: 'home')]
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $allStocks = $this->stockRepository->findAll();
--- var/cache/dev/url_generating_routes.php
+++ var/cache/dev/url_generating_routes.php
@@ -17,7 +17,7 @@ return [
     '_preview_error' => [['code', '_format'], ['_controller' => 'error_controller::preview', '_format' => 'html'], ['code' => '\\d+'], [['variable', '.', '[^/]++', '_format', true], ['variable', '/', '\\d+', 'code', true], ['text', '/_error']], [], [], []],
     'battle_accept_request' => [['battle'], ['_controller' => 'App\\Controller\\BattleController::accept'], [], [['variable', '/', '[^/]++', 'battle', true], ['text', '/battle/accept']], [], [], []],
     'battle_decline_request' => [['battle'], ['_controller' => 'App\\Controller\\BattleController::decline'], [], [['variable', '/', '[^/]++', 'battle', true], ['text', '/battle/decline']], [], [], []],
-    'home' => [[], ['_controller' => 'App\\Controller\\MainController::index'], [], [['text', '/']], [], [], []],
+    'home' => [[], ['_controller' => 'App\\Controller\\MainController::index'], [], [['text', '/home']], [], [], []],
     'app_login' => [[], ['_controller' => 'App\\Controller\\SecurityController::login'], [], [['text', '/login']], [], [], []],
     'app_logout' => [[], ['_controller' => 'App\\Controller\\SecurityController::logout'], [], [['text', '/logout']], [], [], []],
 ];
